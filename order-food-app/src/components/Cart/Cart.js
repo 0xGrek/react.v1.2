e6d510b665959b859e36cd5d258851a3b908bdec
Serhii <rfrf198231@gmail.com>
2023-01-14 21:33:18 +0000

    add new elements and useContext

--- order-food-app/src/components/Cart/Cart.js
+++ order-food-app/src/components/Cart/Cart.js
@@ -1,15 +1,20 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
-import App from "../../App";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
     const cartItems = (
         <ul className={classes["cart-items"]}>
-            {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-                (item) => (
-                    <li>{item.name}</li>
-                )
-            )}
+            {cartCtx.items.map((item) => (
+                <li>{item.name}</li>
+            ))}
         </ul>
     );
 
@@ -18,7 +23,7 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button
@@ -27,7 +32,7 @@ const Cart = (props) => {
                 >
                     Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
